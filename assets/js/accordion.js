e9f6f1243a306a4e35e326f7a187f7e03424db70
Marvin Santiago <marvin.santiago@ymail.com>
2015-07-23 22:17:21 -0500

    Added slideup/slidedown method when button clicked

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -1,3 +1,7 @@
-$('.headers').click(function() {
-  alert('hey');
+var buttonsEl = $('headers');
+var subMenu = $('submenu');
+
+$('.headers').click(function(){
+$('.submenu').slideUp("slow", function(){});
+$(this).next().slideDown("slow", function(){});
 });
