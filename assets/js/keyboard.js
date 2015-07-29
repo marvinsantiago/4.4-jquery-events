398f4e15e9e96e7ca4c9a245164b3a255b114603
Marvin Santiago <marvin.santiago@ymail.com>
2015-07-28 22:31:36 -0500

    Added function to keyboard, delete/shift/caps lock functioning

--- assets/js/keyboard.js
+++ assets/js/keyboard.js
@@ -1,5 +1,22 @@
 var buttonEl = $('button');
+var output = $('.output-screen');
 
-buttonEl.click(function() {
+output.html('');
+
+buttonEl.on('click', function() {
+  if (this.innerHTML === 'tab') {
+    ouput.append(' ');
+  } else if (this.innerHTML === '') {
+    output.append(' ');
+  } else if (this.innerHTML === 'delete') {
+    var newInput = output.html().slice(0, -1);
+    output.html(newInput);
+  } else if (this.innerHTML === 'caps lock' || this.innerHTML === 'shift') {
+    return false;
+  } else if (this.innerHTML === 'return') {
+    output.append('\n');
+  } else {
+    output.append(this.innerHTML);
+  }
 
 });
